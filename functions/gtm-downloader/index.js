0ba80724fd1f6cbd69b64612466789b2e2bc10e4
Jaylin Hicks <jaylin.hicks@searchdiscovery.com>
2023-03-01 21:22:43 +0000

    returning list of containers

--- functions/gtm-downloader/index.js
+++ functions/gtm-downloader/index.js
@@ -11,7 +11,6 @@ const { google } = require('googleapis');
 const tagmanager = google.tagmanager('v2');
 
 
-
 async function main() {
 
   // auth constructor
@@ -19,47 +18,113 @@ async function main() {
     scopes: ['https://www.googleapis.com/auth/tagmanager.readonly'],
     keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS
   });
-  
+
   // create auth client
   const authClient = await auth.getClient();
 
-  // set argument for request (?)
-  const request = { auth: authClient };
+  google.options({
+    // All requests made with this object will use these settings unless overridden.
+    auth: authClient
+  });
+
+  const bqClient = new BigQuery();
 
   /**
    * Get a list of all accounts
    * @see https://developers.google.com/tag-platform/tag-manager/api/v2/reference
    */
-  const accountsList = await tagmanager.accounts.list(request);
+
+  const accountsList = await tagmanager.accounts.list();
   const accounts = accountsList.data.account || [];
 
+  console.log('accounts:',accounts)
+
+  const promises = [];
+
+  accounts.forEach(account => {
+    promises.push(tagmanager.accounts.containers.list({ parent: account.path }))
+  });
+  
+  console.log(promises);
+  
+  const containersList = await Promise.all(promises);
+  
+  console.log(containersList.map(container => container.data.container.map(c => c.name)));
+  
   /**
    * Get a list of containers for each account
    */
-  const containersList = await accounts.reduce(async (acc, account) => {
-    await wait(4000);
-    const containers = await tagmanager.accounts.containers.list({ auth: authClient, parent: account.path });
-    acc = [...containers.data.container];
-    return acc;
-  }, []);
+  // const containersList = accounts.reduce(async (acc, account) => {
+  //   const containers = await Promise.all(
+  //     wait(4000),
+  //     tagmanager.accounts.containers.list({ parent: account.path })
+  //   );
+  //   acc = [...containers.data.container];
+  //   return acc;
+  // }, []);
 
-  const query = containersList.map(c => {
-    c.features = JSON.stringify(c.features);
-    return c;
-  })
+//   get a list of tags 
+  
+  // const liveContainers = containersList.reduce(async (acc, container) => {
+  //   await wait(4000);
+  //   const liveContainers = await tagmanager.accounts.containers.versions.live({ parent: container.path });
+  //   acc = [...liveContainers.data.version];
+  //   return acc;
+  // }, []);
+
+  // const tagsList = await accounts.reduce(async (acc, workspace) =>{
+  //   await wait(4000)
+  //   const tags = await tagmanager.accounts.containers.workspaces.tags.list({ parent: workspace.path})
+  //   acc = [...tags.data.tag];
+  //   return acc;
+  // }, []);
+
+  //   get a list of variables 
+  
+  // const variablesList = await accounts.reduce(async (acc, account) =>{
+  //   await wait(4000)
+  //   const variables = await tagmanager.accounts.containers.workspaces.variables.list({ parent: workspace.path })
+  //   // console.log("variables: ", variables)
+  //   acc = [...variables.data.variable];
+  // })
+
+  // const query = containersList.map(c => {
+  //   c.features = JSON.stringify(c.features);
+  //   return c;
+  // })
+
+  // const tagsQuery = tagsList.map(c => {
+  //   c.features = JSON.stringify(c.features);
+  //   return c;
+  // })
+
+  // const variablesQuery = variablesList.map(c => {
+  //   c.features = JSON.stringify(c.features);
+  //   return c;
+  // })
 
   /**
    * Sends rows to BigQuery
    */
 
-  const bqClient = new BigQuery();
+  // await bqClient
+  //   .dataset('test_gtm_upload')
+  //   .table('test_gtm_containers')
+  //   .insert(query);
 
-  await bqClient
-    .dataset('test_gtm_upload')
-    .table('test_gtm_containers')
-    .insert(query);
+  // await bqClient
+  // .dataset('test_gtm_upload')
+  // .table('test_gtm_tags')
+  // .insert(tagsQuery);
 
-  return query;
+  // await bqClient
+  // .dataset('test_gtm_upload')
+  // .table('test_gtm_variables')
+  // .insert(variablesQuery);
+  
+  // return query;
+  // return tagsQuery;
+  // return liveContainers;
 
 }
 
@@ -74,7 +139,9 @@ functions.http('gtmDownloader', async (req, res) => {
 
     // write to BQ tables
 
+
   } catch(e) {
+    console.error('Something went wrong:',e);
     res.status(500).send(e);
   }
   
