8609da6ec7f735a12861ccb49bf2faa0f7ad8e41
Charlie Tysse <tyssejc@gmail.com>
2023-03-05 14:11:55 +0000

    strict mode and link for pipe module

--- functions/gtm-downloader/helpers/pipe.js
+++ functions/gtm-downloader/helpers/pipe.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// @see https://www.freecodecamp.org/news/pipe-and-compose-in-javascript-5b04004ac937/
 exports.pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 
 exports.asyncPipe = (...fns) => (x) => fns.reduce((p, f) => p.then(f), Promise.resolve(x));
